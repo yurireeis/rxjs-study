1c398ec117d057b98b9ec38003199533f2dea0f8
Yuri Reis <yuri.reis@kch.com.br>
2019-01-29 14:29:00 -0200

    Observables from scratch

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,22 @@
 // import * as $ from 'jquery';
-import { from } from 'rxjs';
-
-const nums = [33, 44, 55, 66, 77];
-const nums$ = from(nums);
+import { Observable } from 'rxjs';
 
 
-// observable from arrays
-nums$.subscribe(
-  value => console.log(value),
-  err => console.log(err),
-  () => console.log('completed')
-);
+// creating observables from scratch
+const source$ = new Observable(observer => {
+  console.log('creating observable...');
+  observer.next('a value');
+  observer.next('another value');
+  observer.error(new Error('Error: something is wrong'));
 
+  setTimeout(() => {
+    observer.next('hello world');
+    observer.complete();
+  }, 2000);
+});
 
-// observable from objects
-const users = [
-  { name: 'Yuri Reis', email: 'yuri.reis@training.com' },
-  { name: 'Reis Yuri', email: 'yuri.reis@training.com' },
-  { name: 'Luiz Silva', email: 'yuri.reis@training.com' },
-];
-const users$ = from(users);
-
-users$.subscribe(
-  value => console.log(value),
-  err => console.log(err),
-  () => console.log('completed')
-);
-
-
-// observable from sets
-const powers = new Set(['Fly', 100, 'neutral']);
-const powers$ = from(powers);
-
-powers$.subscribe(
-  value => console.log(value),
-  err => console.log(err),
-  () => console.log('completed')
-);
-
-// observable from map
-const pairOfNumbers = new Map([[1, 2], [3, 4], [5, 6]]);
-const pairOfNumbers$ = from(pairOfNumbers);
-
-pairOfNumbers$.subscribe(
-  value => console.log(value),
-  err => console.log(err),
-  () => console.log('completed')
-);
-
-
-// observable from string
-const name = 'yuri';
-const name$ = from(name);
-
-name$.subscribe(
-  value => console.log(value),
-  err => console.log(err),
-  () => console.log('completed')
+source$.subscribe(
+  (value) => { console.log(value); },
+  (err) => { console.log(err); },
+  () => { console.log('complete'); }
 );
