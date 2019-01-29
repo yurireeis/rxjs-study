b06289303cdfd233a485f4e38662d2395dde2c01
Yuri Reis <yuri.reis@kch.com.br>
2019-01-29 18:27:28 -0200

    Filter, DebounceTime & Distinct

--- src/app.ts
+++ src/app.ts
@@ -1,82 +1,65 @@
 import * as $ from 'jquery';
-import { Observable } from 'rxjs';
-import { first, last, single, find, findIndex, take, skip, skipWhile, takeWhile } from 'rxjs/operators';
+import { range, from, of } from 'rxjs';
+import { filter, debounceTime, distinct, distinctUntilChanged } from 'rxjs/operators';
 
 function getUserByUsername(username: string): Promise<any> {
   return $.ajax({ url: `https://api.github.com/users/${username}`, dataType: 'jsonp' })
 }
 
-const source$ = new Observable(observer => {
-  observer.next(1);
-  observer.next(2);
-  observer.next('hello');
-  observer.next('world');
-  observer.complete();
-});
+const products = [
+  {
+    sku: '001',
+    name: 'red t-shirt',
+    price: 4.99
+  },
+  {
+    sku: '002',
+    name: 'blue t-shirt',
+    price: 30.35
+  },
+  {
+    sku: '003',
+    name: 'black t-shirt',
+    price: 40.99
+  },
+  {
+    sku: '004',
+    name: 'white t-shirt',
+    price: 10.35
+  }
+];
 
+// filtering only odd values
+range(0, 10)
+  .pipe(filter(x => x % 2 === 0))
+  .subscribe(
+    value => console.log(value),
+    err => console.log(err),
+    () => console.log('completed!')
+  );
 
-// get only the first value
-source$
-  .pipe(first())
+from(products)
+  .pipe(filter(value => value.price > 20.00))
+  .subscribe(products => { console.log(products); });
+
+// filtering w/ debounce time (i.e: don't hit api several times when is typing)
+range(0, 10)
+  .pipe(
+    filter(x => x % 2 === 0),
+    debounceTime(2000)
+  )
+  .subscribe(
+    value => console.log(value),
+    err => console.log(err),
+    () => console.log('completed!')
+  );
+
+// distinct validates if has repeated values (and don't propagates if so)
+of(23, 23, 55, 43, 67, 55, 44, 23, 52)
+  .pipe(distinct())
   .subscribe(value => console.log(value));
 
-// get only the last value
-source$
-  .pipe(last())
-  .subscribe(value => console.log(value));
-
-// get a single value (raise an error if has multiple value)
-source$
-.pipe(single())
-.subscribe(
-  value => console.log(value),
-  err => console.log('error: has multiple values!')
-);
-
-// emits only the first value emitted by the source observable that meets some condition
-source$
-.pipe(find(value => value === 1))
-.subscribe(
-  value => console.log(value),
-  err => console.log('error: has multiple values!')
-);
-
-// find by index
-source$
-.pipe(findIndex(i => i === 0))
-.subscribe(
-  value => console.log(value),
-  err => console.log('error: has multiple values!')
-);
-
-// find by defined limit (in this case is two)
-source$
-.pipe(take(2))
-.subscribe(
-  value => console.log(value),
-  err => console.log('error: has multiple values!')
-);
-
-// start by defined offset (in this case is two)
-source$
-.pipe(skip(2))
-.subscribe(
-  value => console.log(value),
-  err => console.log('error: has multiple values!')
-);
-
-// skip value by defined condition
-source$
-.pipe(skipWhile(value => Boolean(value instanceof String)))
-.subscribe(
-  value => console.log(value),
-  err => console.log('error: has multiple values!')
-);
-
-// take value by defined condition
-source$
-.pipe(takeWhile(value => Boolean(value instanceof Number)))
-.subscribe(
-  value => console.log(value),
-  err => console.log('error: has multiple values!')
-);
+// distinct validates if has repeated values (and propagates if is not in a row)
+of(23, 23, 55, 43, 67, 55, 44, 23, 52)
+.pipe(distinctUntilChanged())
+.subscribe(value => console.log(value));
