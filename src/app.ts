ab935de4c6eca36b311e907828d22ffb5fc1bded
Yuri Reis <yuri.reis@kch.com.br>
2019-01-29 17:01:54 -0200

    Working w/ buffers

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,42 @@
 import * as $ from 'jquery';
-import { interval, fromEvent, from, defer } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-const input = $('#input');
-const length = $('#length');
-const userInfoButton = $('#user-info-button');
-
-// map applies a function to each item that has been emitted
-interval(1000).pipe(map((v) => { v = v * 2 }));
-
-fromEvent(input, 'keyup')
-  .pipe(
-    map(event => (<HTMLInputElement>event.target).value)
-  )
-  .subscribe(
-    value => {
-      console.log(value);
-      length.text(value.length);
-    }
-  );
+import { interval, range, from } from 'rxjs';
+import { buffer, bufferCount, bufferTime } from 'rxjs/operators';
 
 function getUserByUsername(username: string): Promise<any> {
   return $.ajax({ url: `https://api.github.com/users/${username}`, dataType: 'jsonp' })
 }
 
-from(getUserByUsername('yurireeis')).pipe(
-  map((obj: any) => obj.data)
+interval(500).pipe(
+  buffer(interval(2000))
 ).subscribe(
-  userInfo => { console.log(userInfo);
-});
+  value => console.log(value),
+  err => console.log(err),
+  () => console.log('completed!')
+);
+
+range(1, 100)
+  .pipe(bufferCount(4))
+  .subscribe(
+    value => console.log(value),
+    err => console.log(err),
+    () => console.log('completed!')
+  );
+
+interval(1000).pipe(
+  bufferTime(5000)
+)
+.subscribe(
+  value => console.log(value),
+  err => console.log(err),
+  () => console.log('completed!')
+);
+
+const obs1$ = interval(1000);
+const obs2$ = from(document, 'click');
+
+const myBuffer = obs1$.pipe(buffer(obs2$));
+const subscribe = myBuffer.subscribe(
+  value => console.log(value),
+  err => console.log(err),
+  () => console.log('completed!')
+);
