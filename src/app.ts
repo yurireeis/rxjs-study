0ed92a662098ea9a751501510bca525e8ddd71e5
Yuri Reis <yuri.reis@kch.com.br>
2019-01-29 17:55:04 -0200

    Find & select from observables

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,82 @@
 import * as $ from 'jquery';
-import { interval, range, from } from 'rxjs';
-import { buffer, bufferCount, bufferTime } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { first, last, single, find, findIndex, take, skip, skipWhile, takeWhile } from 'rxjs/operators';
 
 function getUserByUsername(username: string): Promise<any> {
   return $.ajax({ url: `https://api.github.com/users/${username}`, dataType: 'jsonp' })
 }
 
-interval(500).pipe(
-  buffer(interval(2000))
-).subscribe(
-  value => console.log(value),
-  err => console.log(err),
-  () => console.log('completed!')
-);
+const source$ = new Observable(observer => {
+  observer.next(1);
+  observer.next(2);
+  observer.next('hello');
+  observer.next('world');
+  observer.complete();
+});
 
-range(1, 100)
-  .pipe(bufferCount(4))
-  .subscribe(
-    value => console.log(value),
-    err => console.log(err),
-    () => console.log('completed!')
-  );
 
-interval(1000).pipe(
-  bufferTime(5000)
-)
+// get only the first value
+source$
+  .pipe(first())
+  .subscribe(value => console.log(value));
+
+// get only the last value
+source$
+  .pipe(last())
+  .subscribe(value => console.log(value));
+
+// get a single value (raise an error if has multiple value)
+source$
+.pipe(single())
 .subscribe(
   value => console.log(value),
-  err => console.log(err),
-  () => console.log('completed!')
+  err => console.log('error: has multiple values!')
 );
 
-const obs1$ = interval(1000);
-const obs2$ = from(document, 'click');
-
-const myBuffer = obs1$.pipe(buffer(obs2$));
-const subscribe = myBuffer.subscribe(
+// emits only the first value emitted by the source observable that meets some condition
+source$
+.pipe(find(value => value === 1))
+.subscribe(
   value => console.log(value),
-  err => console.log(err),
-  () => console.log('completed!')
+  err => console.log('error: has multiple values!')
+);
+
+// find by index
+source$
+.pipe(findIndex(i => i === 0))
+.subscribe(
+  value => console.log(value),
+  err => console.log('error: has multiple values!')
+);
+
+// find by defined limit (in this case is two)
+source$
+.pipe(take(2))
+.subscribe(
+  value => console.log(value),
+  err => console.log('error: has multiple values!')
+);
+
+// start by defined offset (in this case is two)
+source$
+.pipe(skip(2))
+.subscribe(
+  value => console.log(value),
+  err => console.log('error: has multiple values!')
+);
+
+// skip value by defined condition
+source$
+.pipe(skipWhile(value => Boolean(value instanceof String)))
+.subscribe(
+  value => console.log(value),
+  err => console.log('error: has multiple values!')
+);
+
+// take value by defined condition
+source$
+.pipe(takeWhile(value => Boolean(value instanceof Number)))
+.subscribe(
+  value => console.log(value),
+  err => console.log('error: has multiple values!')
 );
