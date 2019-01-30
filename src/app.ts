8447d66f650795d22ed1b88178a8c58eba7e7379
Yuri Reis <yuri.reis@kch.com.br>
2019-01-30 09:42:40 -0200

    Mergemap, concatMap & switchMap

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,28 @@
-import { of, interval, merge, from, concat } from 'rxjs';
-import { map, merge as mergeOperator  } from 'rxjs/operators';
+import { of, interval, merge, from, concat, fromEvent } from 'rxjs';
+import { mergeMap  } from 'rxjs/operators';
 
-// merging two observables
-of([23233])
+// using mergeMap to use the result of a promise in another promise
+of('hello')
   .pipe(
-    mergeOperator(
-      of([213412515])
-    )
+    mergeMap(v => of(`${v} world`))
   )
-  .subscribe(([firstValue, secondValue]) => console.log(firstValue, secondValue));
+  .subscribe(value => console.log(value));
 
-  // call merge directly passing observables as params
-  const obs1$ = interval(2000).pipe(map(i => `M1: ${i}`));
-  const obs2$ = interval(5000).pipe(map(i => `M2: ${i}`));
+// a better example using a promise
+const myPromise = function (v: any) {
+  return new Promise((resolve, reject) => {
+    resolve(`${v} world from promise`);
+  })
+}
 
-  const merged = merge(obs1$, obs2$).subscribe(result => {
-    console.log(result);
-  });
+of('hello')
+  .pipe(
+    mergeMap(v => myPromise(v))
+  )
+  .subscribe(value => console.log(value));
 
-  // concatenating values
-  const obs3$ = from([33, 44, 55]);
-  const obs4$ = from([66, 77, 88]);
 
-  concat(obs3$, obs4$).subscribe(
-    value => console.log('concat: ' + value)
-  );
+  const button = $('#button');
+
+  fromEvent(button, 'click')
+    .subscribe(value => { console.log(value); });
