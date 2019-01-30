78ac6f3a859174366b09cdff92d13d9c7a4ca5f1
Yuri Reis <yuri.reis@kch.com.br>
2019-01-30 14:02:24 -0200

    Utility operators (delayWhen, delay, every, defaultIfEmpty and so on...)

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,72 @@
-import * as $ from 'jquery';
-import { fromEvent, from, of, zip, interval, timer, combineLatest } from 'rxjs';
-import { map, distinct, debounceTime, withLatestFrom, tap, mergeMap, catchError } from 'rxjs/operators';
-
-const source1$ = of('first');
-const source2$ = of('second');
-const source3$ = of('third');
-const source4$ = of('fourth');
+import { of, merge, interval, timer, from } from 'rxjs';
+import { map, tap, defaultIfEmpty, every, mapTo, delay, delayWhen, timeInterval } from 'rxjs/operators';
 
 function getSubscriber(_type: string) {
   return {
-    next (value: any) { console.log(`success ${_type}`, value); },
-    error (err: any) { console.log('error: ', err); },
-    complete () { console.log('completed') },
-  }
+    next (value: any) { console.log(`${_type} completed successfully: ${value}`); },
+    error (err: any) { console.log(err); },
+    complete () { console.log(`${_type} completed`); }
+  };
 }
 
-// using zip, when the first arbitrary observable ends, everyone stops too
-zip(
-  source1$,
-  source2$,
-  source3$,
-  source4$
-).subscribe(getSubscriber('zip'));
+// default if empty
+const source$ = of();
 
-// withLatestFrom works with following rule: when the observable defined in withLatestFrom ends, everyone combined with it ends too
-const interval1$ = interval(1000);
-const interval2$ = interval(5000);
-
-interval1$
+source$
   .pipe(
-    withLatestFrom(interval2$)
-  ).subscribe(getSubscriber('withLatestFrom'));
+    defaultIfEmpty('my default value')
+  )
+  .subscribe(getSubscriber('defaultIfEmpty'));
 
-const timer1$ = timer(1000, 3);
-const timer2$ = timer(1000, 10);
-const timer3$ = timer(1000, 5);
+// every checks if all values emitted satisfies some condition
 
-combineLatest(
-  timer1$,
-  timer2$,
-  timer3$
-).subscribe(([timer1, timer2, timer3]) => {
-  console.log(`
-    timer one latest: ${timer1},
-    timer two latest: ${timer2},
-    timer three latest: ${timer3},
-  `.toUpperCase());
-});
+const source2$ = of(1, 2, 3);
+
+source2$
+  .pipe(
+    every(value => value % 2 === 0)
+  )
+  .subscribe(getSubscriber('every'));
+
+// do simply do something (don't emit values) (now is tap!)
+
+const source3$ = of(1, 2, 3, 4)
+
+source3$.pipe(
+  tap(value => console.log('BEFORE MAP: ' + value)),
+  map(value => value *2),
+  tap(value => console.log('AFTER MAP: ' + value))
+).subscribe(getSubscriber('tap/do'));
+
+
+// delay
+const source4$ = of(null);
+
+merge(
+  source4$.pipe(
+    mapTo('First')
+  ),
+  source4$.pipe(
+    delay(5000),
+    mapTo('Second')
+  ),
+  source4$.pipe(
+    mapTo('Third'),
+    delay(3000),
+  ),
+  source4$.pipe(
+    mapTo('Fourth'),
+    delay(5000),
+  )
+).subscribe(getSubscriber('delay'));
+
+
+// delayWhen
+const source5$ = interval(1000);
+
+
+source5$
+  .pipe(
+    delayWhen(() => timer(5000))
+  )
+  .subscribe(getSubscriber('delayWhen'));
